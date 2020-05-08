707a9ff5aa612c9eb10d51119f54e062adbdab48
djzhang <trujunzhang@gmail.com>
2020-05-08 16:50:14 +0800

    AuthGoogleBtn

--- lib/src/ui/auth/widgets/auth_google_btn.dart
+++ lib/src/ui/auth/widgets/auth_google_btn.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'package:ieatta/src/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class AuthGoogleBtn extends StatelessWidget {
-  const AuthGoogleBtn({Key key, this.isSignIn}) : super(key: key);
+class AuthGoogleBtn extends StatefulWidget {
+  AuthGoogleBtn({Key key, this.isSignIn}) : super(key: key);
 
   final bool isSignIn;
+
+  @override
+  _AuthGoogleBtnState createState() => _AuthGoogleBtnState();
+}
+
+class _AuthGoogleBtnState extends State<AuthGoogleBtn> {
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +35,14 @@ class AuthGoogleBtn extends StatelessWidget {
         height: 48,
         padding: const EdgeInsets.only(top: 8),
         child: GoogleSignInButton(
-          text: isSignIn ? 'Sign in with Google' : 'Sign up with Google',
+          text: widget.isSignIn ? 'Sign in with Google' : 'Sign up with Google',
           onPressed: onClick,
           darkMode: true, // default: false
         ));
   }
 
   onClick() {
-    var x = 0;
+
+
   }
 }
